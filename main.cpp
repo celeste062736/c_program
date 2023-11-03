1df296d11b6db452936ce8ac3fac4cb6c4ce1ffe
MinGeun Kim <skykimmg0627@gmail.com>
2023-11-03 07:28:43 +0000

    add

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int main(void) {
     Box a(10, 10, 10), b(20, 20, 20), c;
 
+    //1
     c = a + b;
     printBox(c);
 
+    //2
     cout << (a == b) << endl;
 
+    //3
     printBox(a);
     return 0;
 }
